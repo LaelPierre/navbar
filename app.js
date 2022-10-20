104461b040d68a47937717ac4284996bfeb81012
Lael Pierre <lael.aichaoui@gmail.com>
2022-10-19 20:10:39 -0700

    toggle functionnality added and tested

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+// select button and link class
+const btnToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
 
+// add toggle functionnality
+btnToggle.addEventListener("click", function () {
+  links.classList.toggle("show-links");
+});
